2628b3f0bc8cae126a191e4bb99efc805d3a127b
Tatiane <tatianemedeirosss@gmail.com>
2019-04-17 02:15:31 -0300

    added models and initial migration
    Signed-off-by: Tatiane <tatianemedeirosss@gmail.com>

--- tccStudentRegistration/admin.py
+++ tccStudentRegistration/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(FormaEvasao)
+
+
+admin.site.register(FormaIngresso)
+
+
+admin.site.register(Disciplina)
+
+
+admin.site.register(Curso)
+
+
+admin.site.register(SituacaoMatricula)
+
+
+admin.site.register(Aluno)
+
+
+admin.site.register(Matricula)
--- tccStudentRegistration/models.py
+++ tccStudentRegistration/models.py
@@ -4,13 +4,178 @@ from django.db import models
 # Create your models here.
 
 
-class FormaEvasao(object):
-    """FormaEvasao model."""
+class FormaEvasao(models.Model):
+    """FormaEvasao objects model.
 
-    descricao_evasao = models.CharField(max_length=255)
+    Used to keep track of FormaEvasao objects information.
+    """
+
+    descricao_evasao = models.CharField(
+        max_length=255,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+
+    def __str__(self):
+        return self.descricao_evasao
 
 
-class FormaIngresso(object):
-    """FormaIngresso model."""
+class FormaIngresso(models.Model):
+    """FormaIngresso objects model.
 
-    descricao_ingresso = models.CharField(max_length=255)
+    Used to keep track of FormaIngresso objects information.
+    """
+
+    descricao_ingresso = models.CharField(
+        max_length=255,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+
+    def __str__(self):
+        return self.descricao_ingresso
+
+
+class Disciplina(models.Model):
+    """Disciplina objects model.
+
+    Used to keep track of Disciplina objects information.
+    """
+
+    codigo_disciplina = models.CharField(
+        max_length=45,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    descricao_disciplina = models.CharField(
+        max_length=45,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    carga_horaria = models.CharField(
+        max_length=45,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+
+    def __str__(self):
+        return self.descricao_disciplina
+
+
+class Curso(models.Model):
+    """Curso objects model.
+
+    Used to keep track of Curso objects information.
+    """
+
+    codigo_curso = models.CharField(
+        max_length=45,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    descricao_curso = models.CharField(
+        max_length=255,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    disciplinas = models.ManyToManyField(
+        Disciplina,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+
+    def __str__(self):
+        return self.descricao_curso
+
+
+class SituacaoMatricula(models.Model):
+    """SituacaoMatricula objects model.
+
+    Used to keep track of SituacaoMatricula objects information.
+    """
+
+    descricao_situacao_matricula = models.CharField(
+        max_length=100,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+
+    def __str__(self):
+        return self.descricao_situacao_matricula
+
+
+class Aluno(models.Model):
+    """Aluno objects model.
+
+    Used to keep track of Aluno objects information.
+    """
+
+    grr_aluno = models.CharField(
+        max_length=45,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    nome_aluno = models.CharField(
+        max_length=255,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    periodo_ingresso = models.DateField(
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    periodo_evasao = models.DateField(
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    forma_ingresso = models.ForeignKey(
+        FormaIngresso,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        on_delete=models.CASCADE
+        )
+    forma_evasao = models.ForeignKey(
+        FormaEvasao,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        on_delete=models.CASCADE
+        )
+
+    def __str__(self):
+        return self.nome_aluno
+
+
+class Matricula(models.Model):
+    """Matricula objects model.
+
+    Used to keep track of Matricula objects information.
+    """
+
+    media_final_matricula = models.FloatField(
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    faltas_matricula = models.IntegerField(
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    periodo_matricula = models.DateField(
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    alunos = models.ManyToManyField(
+        Aluno,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    disciplinas = models.ManyToManyField(
+        Disciplina,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        )
+    situacao_matricula = models.ForeignKey(
+        SituacaoMatricula,
+        # verbose_name=_('CLLB201'),
+        # help_text=_('CLHT201'),
+        on_delete=models.CASCADE
+        )
