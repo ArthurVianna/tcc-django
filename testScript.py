09f8c046feecfa43e67b7921114a8fdc095d5fb0
Arthur Vianna <arthurvlandeo@gmail.com>
2019-10-04 19:37:58 -0300

    Increased the precision of Evasao prediction

--- testScript.py
+++ testScript.py
@@ -8,27 +8,125 @@ import django
 django.setup()
 
 # your imports, e.g. Django models
-#from tccStudentRegistration.models import *
-# from tccStudentRegistration.importCSV import *
+from tccStudentRegistration.models import *
+#from tccStudentRegistration.importCSV import *
 #from datawarehouseManager.dataMining import *
 from datawarehouseManager.models import *
+
 from sklearn.preprocessing import StandardScaler
 from pandas import pandas as pd
 scaler = StandardScaler()
 print("working")
 
+reprovado = ['Reprovado por nota'
+, 'Reprovado por Frequência'
+, 'Cancelado'
+, 'Incompleto'
+, 'Reprovado sem nota'
+, 'Matrícula'
+, 'Trancamento Total'
+, 'Disciplina sem Oferta'
+, 'Trancamento Administrativo'
+, 'PROVAR'
+, 'Trancamento CEPE'
+, 'Trancamento Outra IES'
+, 'Reprov Conhecimento'
+, 'Reprov Adiantamento'
+, 'Trancamento por amparo Legal'
+, 'Isento por Transferência'
+, 'Desistência de Vaga'
+, 'Processo Administrativo'
+, 'Horas']
+
+
+
+evadiu = []
+
+
+
+#'Reprovado por nota'
+#'Reprovado por Frequência'
+#'Cancelado'
+#'Trancamento Total'
+#'Reprovado sem nota'
+#'Incompleto'
+#'Reprov Conhecimento'
+#'Reprov Adiantamento'
+
+
 fatoEvasaoLista = FatoEvasao.objects.filter(alunoEvasao__isnull=False,situacaoEvasao__isnull=False,cursoEvasao__isnull=False,semestreEvasao__isnull=False)
 df = pd.DataFrame(list(fatoEvasaoLista.values()))
+
+
+	#alunoMatricula__isnull=False,
+    #situacaoMatricula__isnull=False,
+    #disciplinaMatricula__isnull=False,
+    #cursoMatricula__isnull=False,
+    #semestreMatricula__isnull=False)
+listaRetencoes = []
+maxCount = 0
+for i , j in df.iterrows():
+	#print(i,j)
+	#print(j["alunoEvasao_id"])
+	retencoes = FatoMatricula.objects.filter(alunoMatricula__id=j["alunoEvasao_id"],situacaoMatricula__descricao_situacao_matricula__in=reprovado
+		,disciplinaMatricula__isnull=False
+		,cursoMatricula__isnull=False
+		,semestreMatricula__isnull=False)
+	listaDisciplinasReprovadas = []
+	count = 0 
+	for x in retencoes:
+		#if(x.situacaoMatricula.descricao_situacao_matricula in ['Trancamento Total']):
+		listaDisciplinasReprovadas += [x.disciplinaMatricula.pk]
+		count+=1
+		#print(x.disciplinaMatricula.codigo_disciplina)
+	if(count > maxCount):
+		maxCount = count
+	#print(retencoes)
+	
+	listaRetencoes += [listaDisciplinasReprovadas]
+	#print(listaRetencoes)
+	#print(count)
+
+retencoesFloat = []
+for i in listaRetencoes:
+	somador = 0
+	for j in i:
+		somador = j
+	retencoesFloat += [somador/maxCount]
+
+mask = df.situacaoEvasao_id != 1
+column_name = 'situacaoEvasao_id'
+df.loc[mask, column_name] = 3
+		
+			
+
+
 df = df.drop("coeficienteEvasao",axis=1)
 df = df.drop("alunoEvasao_id",axis=1)
 df = df.drop("cursoEvasao_id",axis=1)
 df = df.drop("semestreEvasao_id",axis=1)
 df = df.drop("quantidadeEvasao",axis=1)
+
 df = df.drop("id",axis=1)
+df['Retencoes'] = retencoesFloat
+
+mask = df.situacaoEvasao_id != 1
+column_name = 'situacaoEvasao_id'
+df.loc[mask, column_name] = 3
+#df = df.astype({"Retencoes": float})
 print(df)
-scaler.fit(df.drop("situacaoEvasao_id",axis=1))
-scaled_features = scaler.transform(df.drop("situacaoEvasao_id",axis=1))
-df_feat = pd.DataFrame(scaled_features,columns=df.columns[:-1])
+#df.
+
+
+#print(df)
+variavelqqr = df.drop("situacaoEvasao_id",axis=1)
+print("variavel qqr = " , variavelqqr)
+print(variavelqqr.shape)
+print("df shape = ", df.shape )#.astype({"Retencoes": float})
+scaler.fit(variavelqqr)
+scaled_features = scaler.transform(variavelqqr)#.astype({"Retencoes": float}))
+print("Scaled features", scaled_features)
+df_feat = pd.DataFrame(scaled_features,columns=['quantidadeRetencoes','ira','Retencoes'])
 print(df_feat)
 
 from sklearn.model_selection import train_test_split
@@ -50,3 +148,6 @@ from sklearn.metrics import classification_report,confusion_matrix
 
 print(confusion_matrix(y_test,pred))
 print(classification_report(y_test,pred))
+
+
+
