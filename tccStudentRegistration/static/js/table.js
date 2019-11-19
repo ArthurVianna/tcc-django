a25c27dd2bdc2e95ed0f441298fd0575f57d4991
Tatiane <tatianemedeirosss@gmail.com>
2019-11-19 20:56:36 -0300

    bug fix
    Signed-off-by: Tatiane <tatianemedeirosss@gmail.com>

--- tccStudentRegistration/static/js/table.js
+++ tccStudentRegistration/static/js/table.js
@@ -1,3 +1,4 @@
+console.log("oi");
 var dataSet = [
     [ "Tiger Nixon", "System Architect", "Edinburgh", "5421", "2011/04/25", "$320,800" ],
     [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750" ],
