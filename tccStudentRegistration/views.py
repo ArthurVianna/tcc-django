e8ec86e430209635830953650e4a5304993fdfae
Tatiane <tatianemedeirosss@gmail.com>
2019-11-25 18:34:18 -0300

    delete comment view
    Signed-off-by: Tatiane <tatianemedeirosss@gmail.com>

--- tccStudentRegistration/views.py
+++ tccStudentRegistration/views.py
@@ -74,7 +74,7 @@ def aluno_detail(request, pk):
     aluno = get_object_or_404(Aluno, pk=pk)
     predicao = PredicaoEvasao.objects.filter(aluno=aluno)
     if predicao:
-        predicao.latest('periodo_predicao')  # noqa
+        predicao.latest('periodo_predicao')
     matricula = Matricula.objects.filter(aluno__id=pk).order_by(
         'periodo_matricula')
     return render(request, 'tcc/aluno_detail.html',
