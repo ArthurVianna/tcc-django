1ad2d234886b14ec3f1f5bd5cdee0c9fee47c33b
Tatiane <tatianemedeirosss@gmail.com>
2019-11-29 21:16:30 -0300

    visual changes
    Signed-off-by: Tatiane <tatianemedeirosss@gmail.com>

--- tccStudentRegistration/views.py
+++ tccStudentRegistration/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.shortcuts import redirect
+from .models import PredicaoEvasao, FormaEvasao
 from .StudentRegistrationFacade import StudentRegistrationFacade
 from .ImportDataFacade import ImportDataFacade
 from .forms import EditarUsuarioForm
